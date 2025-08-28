3010e86f12437e72fafa9ccbaf78b211e44ab360
Felipe Bernardo de Oliveira <felipebernardo55@hotmail.com>
2025-08-28 00:14:20 -0300

    Adicionando testes unitários para o login

---
+++ tests/Unit/LoginTest.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Tests\Unit;
+
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\AuthController;
+use App\Http\Requests\LoginRequest;
+use App\Models\User;
+use Tests\TestCase;
+use Mockery;
+
+class LoginTest extends TestCase
+{
+    public function test_login_failed_user_not_found()
+    {
+        $request = Mockery::mock(LoginRequest::class);
+        $request->shouldReceive('all')
+            ->andReturn([
+                'email' => 'user@email.com',
+                'password' => '123'
+            ]);
+
+        $userModelMock = Mockery::mock(User::class);
+
+        $userModelMock = Mockery::mock(User::class);
+        $userModelMock->shouldReceive('getUserByEmail')
+            ->with('user@email.com')
+            ->andReturn([]);
+
+        $controller = new AuthController($userModelMock);
+
+        $response = $controller->login($request);
+
+        $this->assertEquals(404, $response->status());
+        $this->assertEquals((object)['success' => false, 'error' => 'Usuário não encontrado!'], $response->getData());
+    }
+
+    public function test_login_failed_user_unauthorized()
+    {
+        $userMock = [
+            'email' => 'user@email.com',
+            'password' => '123'
+        ];
+
+        $request = Mockery::mock(LoginRequest::class);
+        $request->shouldReceive('all')
+            ->andReturn($userMock);
+
+        $userModelMock = Mockery::mock(User::class);
+
+        $userModelMock = Mockery::mock(User::class);
+        $userModelMock->shouldReceive('getUserByEmail')
+            ->with('user@email.com')
+            ->andReturn($userMock);
+
+        Auth::shouldReceive('attempt')
+            ->with($userMock)
+            ->andReturn(false);
+
+        $controller = new AuthController($userModelMock);
+
+        $response = $controller->login($request);
+
+        $this->assertEquals(401, $response->status());
+        $this->assertEquals((object)['success' => false, 'error' => 'Não autorizado!'], $response->getData());
+    }
+
+    public function test_login_success()
+    {
+        $userMock = [
+            'email' => 'user@email.com',
+            'password' => '123'
+        ];
+
+        $tokenMock = 'absidj9817312931hkh';
+
+        $request = Mockery::mock(LoginRequest::class);
+        $request->shouldReceive('all')
+            ->andReturn($userMock);
+
+        $userModelMock = Mockery::mock(User::class);
+
+        $userModelMock = Mockery::mock(User::class);
+        $userModelMock->shouldReceive('getUserByEmail')
+            ->with('user@email.com')
+            ->andReturn($userMock);
+
+        Auth::shouldReceive('attempt')
+            ->with($userMock)
+            ->andReturn($tokenMock);
+
+        $controller = new AuthController($userModelMock);
+
+        $response = $controller->login($request);
+
+        $this->assertEquals(200, $response->status());
+        $this->assertEquals((object)['success' => true, 'data' => (object)['token' => $tokenMock]], $response->getData());
+    }
+}
